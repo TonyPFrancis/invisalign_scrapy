231eea85a50202b9a7c3fd3c170008f90b82336b
Jithin <cyb3rjithu@gmail.com>
2015-06-13 19:03:50 +0530

    updating items file with req fields

--- invisalign_scrapy/invisalign_scrapy/items.py
+++ invisalign_scrapy/invisalign_scrapy/items.py
@@ -5,10 +5,14 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Field,Item
 
 
-class InvisalignScrapyItem(scrapy.Item):
+class InvisalignScrapyItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = Field()
+    address = Field()
+    phone = Field()
+    type = Field()
+    category = Field()
